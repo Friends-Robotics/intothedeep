8a36137a6f4a35d4765ef923db522a13241fbcb8
Aaron Portis <aaronjportis@gmail.com>
2025-06-21 13:51:32 +0100

    Adding telemetry for normalised colour sensor

--- TeamCode/src/main/java/friends/teleOp/IntakeTeleOp.java
+++ TeamCode/src/main/java/friends/teleOp/IntakeTeleOp.java
@@ -2,6 +2,7 @@ package friends.teleOp;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 
 import static friends.helper.gamepad.GamepadButton.*;
 
@@ -22,8 +23,8 @@ public class IntakeTeleOp extends LinearOpMode {
 
         GamepadEx primary = new GamepadEx(gamepad1);
 
-        primary.pressed(RIGHT_BUMPER, intake::slideOut);
-        primary.pressed(LEFT_BUMPER, intake::slideIn);
+//        primary.pressed(RIGHT_BUMPER, intake::slideOut);
+//        primary.pressed(LEFT_BUMPER, intake::slideIn);
         primary.pressed(CROSS, intake::cycle);
 
         primary.down(CIRCLE, intake::ready);
@@ -51,18 +52,30 @@ public class IntakeTeleOp extends LinearOpMode {
             telemetry.addData("R:", map.ColorSensor.red());
             telemetry.addData("G:", map.ColorSensor.green());
             telemetry.addData("B:", map.ColorSensor.blue());
-            telemetry.addData("Luminance:", map.ColorSensor.alpha());
+            telemetry.addData("A:", map.ColorSensor.alpha());
 
-            telemetry.addLine("HSV Values");
+            ColorSensor sensor = map.ColorSensor;
+
+            double scalingFactor = 255.0f / sensor.alpha();
+            int red = (int)((double)sensor.red() * scalingFactor);
+            int green = (int)((double)sensor.green() * scalingFactor);
+            int blue = (int)((double)sensor.blue() * scalingFactor);
+
+            telemetry.addLine("Normalised Values");
+            telemetry.addData("Scaling Factor:", 255.0f / map.ColorSensor.alpha());
+            telemetry.addData("R:", red);
+            telemetry.addData("G:", green);
+            telemetry.addData("B:", blue);
+
+            telemetry.addLine("Normalised HSV Values");
             telemetry.addData("H:", hsv[0]);
             telemetry.addData("S:", hsv[1]);
             telemetry.addData("V:", hsv[2]);
 
             telemetry.addData("Currently Selected Colour", intake.getColour().toString());
 
-
-            intake.runSlidePID();
-
+//          intake.runSlidePID();
+            intake.slideOutWithSetPower(-gamepad1.right_stick_y);
             telemetry.update();
         }
     }
